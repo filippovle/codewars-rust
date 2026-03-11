0302d642303e8b6208b1012c124fdc10231ce0ba
Lew1998 <lewfilippov@gmail.com>
2026-03-11 19:45:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# codewars-rust
+
+Мои решения задач с Codewars на Rust.
+
+## Запуск
+
+Проверить всё:
+
+```bash
+cargo test
---
+++ src/katas/mod.rs
@@ -0,0 +1,8 @@
+pub mod kyu1;
+pub mod kyu2;
+pub mod kyu3;
+pub mod kyu4;
+pub mod kyu5;
+pub mod kyu6;
+pub mod kyu7;
+pub mod kyu8;
